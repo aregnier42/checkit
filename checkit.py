ea8b2351cc4d8aedd86246a79d056941f73e232b
aregnier42 <1929487+aregnier42@users.noreply.github.com>
2020-11-10 15:10:36 +0100

    fixed quotes

--- checkit.py
+++ checkit.py
@@ -11,7 +11,7 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         client=str(self.headers.get("X-Forwarded-For","n/a"))+" - " +str(self.client_address[0])
-        response=b'['+datetime.datetime.now().strftime("%Y%m%d-%H%M%S.%f")+’]’+b' key='+key+b' host='+hostname+b' - Hello '+client+' \n'
+        response=b'['+datetime.datetime.now().strftime("%Y%m%d-%H%M%S.%f")+']'+b' key='+key+b' host='+hostname+b' - Hello '+client+' \n'
         self.wfile.write(response)
         print("==> "+response)
 
